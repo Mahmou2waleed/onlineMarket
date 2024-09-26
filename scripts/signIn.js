2fe31cffe2270f3e757e1d5297fa9e85499fa6f4
Mahmoud Waleed <143889370+Mahmou2waleed@users.noreply.github.com>
2024-09-27 00:17:44 +0300

    Add files via upload

--- scripts/signIn.js
+++ scripts/signIn.js
@@ -5,25 +5,24 @@ document.getElementById('sign-in-form').addEventListener('submit', async functio
     const password = document.getElementById('input-password').value;
 
     try {
-        const response = await fetch('https://script.google.com/macros/s/AKfycbzAHqGIizUn4ntwSLvg1B6Lf05b18eEo081qpx8UgIEfv0nt-ek9RAFcLYZRMbHuwXrYQ/exec', { 
+        const response = await fetch('https://script.google.com/macros/s/AKfycbyEitl60n67Zj9rGAZ4ijZhWxgVuuY3k0uEZD9bJPwx9qa766DsK_FW2M-fokxBQ1yaGQ/exec', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ email, password }) // نرسل البريد الإلكتروني وكلمة المرور
+            body: JSON.stringify({ email, password })
         });
 
-        // التحقق من حالة الاستجابة
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        const result = await response.json(); // نحصل على النتيجة بصيغة JSON
+        const result = await response.json();
 
-        // تحقق مما إذا كانت الاستجابة ناجحة
         if (result.success) {
+            localStorage.setItem('userData', JSON.stringify(result));
             alert("Login successful!");
-            // يمكنك إعادة التوجيه إلى صفحة أخرى هنا
+            window.location.href = 'index.html';
         } else {
             alert("Login failed: " + (result.error || 'Invalid email or password.'));
         }
